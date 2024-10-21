f188c730fd36dc283d7065ce977a2cd82bcf5ad3
hp <hp@DESKTOP-1E95RK6>
2024-10-21 11:54:05 +0530

    written methods

--- src/test/java/commonfunction/FuntionLibrary.java
+++ src/test/java/commonfunction/FuntionLibrary.java
@@ -1,5 +1,82 @@
 package commonfunction;
 
-public class FuntionLibrary {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+
+public class FuntionLibrary
+{
+	public static WebDriver driver;
+	public static Properties conpro;
+	public static WebDriver startBrowser() throws Throwable
+	{
+		conpro = new Properties();
+		conpro.load(new FileInputStream("Propertyfiles/Environmental properties"));
+		if(conpro.getProperty("Browser").equalsIgnoreCase("chrome"))
+		{
+			driver= new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		else if(conpro.getProperty("Browser").equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+			
+		}
+		else {
+			Reporter.log("browser value is not matching",true);
+		}
+		return driver;
+	}
+	public static void openUrl() {
+		driver.get(conpro.getProperty("url"));
+		
+	}
+	public static void waitForElement(String LocatorType,String LocatorValue,String TestData) 
+	{
+		WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(TestData)));
+		if(LocatorType.equalsIgnoreCase("xpath")) {
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LocatorValue)));
+		}
+		
+	}
+	public static void typeAction(String LocatorType,String LocatorValue,String TestData)
+	{
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).clear();
+			driver.findElement(By.xpath(LocatorValue)).sendKeys(TestData);
+		}
+		if(LocatorType.equalsIgnoreCase("id")) {
+			driver.findElement(By.id(LocatorValue)).clear();
+			driver.findElement(By.id(LocatorValue)).sendKeys(TestData);
+		}
+	}
+	public static void clickAction(String LocatorType,String LocatorValue) 
+	{
+		if(LocatorType.equalsIgnoreCase("xpath")) {
+			driver.findElement(By.xpath(LocatorValue)).click();
+		}
+	}
+	public static void validateTitle(String Expected_Title)
+	{
+		String Actual_title=driver.getTitle();
+		Assert.assertEquals(Actual_title, Expected_Title,"Title is Not matching ");
+	}
+	public static void closeBrowser() {
+		driver.quit();
+	}
+
+
 
 }
